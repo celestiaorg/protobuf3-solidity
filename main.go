e7773b7a929b927b5f6ea9bc7656c5b665263d6e
John Adler <john.adler@protonmail.ch>
2020-08-29 18:03:21 -0400

    Add protobuf plugin import.

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	plugin "google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
-	// req := &plugin.CodeGeneratorRequest{}
-	// resp := &plugin.CodeGeneratorResponse{}
+	req := &plugin.CodeGeneratorRequest{}
+	resp := &plugin.CodeGeneratorResponse{}
 
 	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
