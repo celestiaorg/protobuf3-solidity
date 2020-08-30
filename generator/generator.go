2842e96c533257ed50e9fcc76eb094fa168da8f7
John Adler <john.adler@protonmail.ch>
2020-08-30 12:36:21 -0400

    Check for proto3 syntax.

--- generator/generator.go
+++ generator/generator.go
@@ -1,6 +1,8 @@
 package generator
 
 import (
+	"errors"
+
 	"google.golang.org/protobuf/types/pluginpb"
 )
 
@@ -21,7 +23,17 @@ func New(request *pluginpb.CodeGeneratorRequest) *Generator {
 
 // Generate generates Solidity code from the requested .proto files.
 func (g *Generator) Generate() (*pluginpb.CodeGeneratorResponse_File, error) {
-	println(g.request.GetFileToGenerate()[0])
+	err := checkSyntaxVersion(g.request.GetProtoFile()[0].GetSyntax())
+	if err != nil {
+		return nil, err
+	}
 
 	return nil, nil
+}
+
+func checkSyntaxVersion(v string) error {
+	if v == "proto3" {
+		return nil
+	}
+	return errors.New("Must use syntax = \"proto3\";")
 }
