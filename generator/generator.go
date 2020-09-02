f2a6cd44c81a6e1bac95a2e92ab23b3390295f0d
John Adler <john.adler@protonmail.ch>
2020-09-02 17:03:26 -0400

    Add empty struct generation.

--- generator/generator.go
+++ generator/generator.go
@@ -2,6 +2,7 @@ package generator
 
 import (
 	"errors"
+	"fmt"
 
 	"google.golang.org/protobuf/types/descriptorpb"
 	"google.golang.org/protobuf/types/pluginpb"
@@ -50,6 +51,13 @@ func generateFile(protoFile *descriptorpb.FileDescriptorProto) (*pluginpb.CodeGe
 
 	b := &WriteableBuffer{}
 
+	// TODO option for license
+	b.P(fmt.Sprintf("// SPDX-License-Identifier: %s", "CC0"))
+	b.P(SolidityVersionString)
+	b.P()
+	b.P("import \"@lazyledger/protobuf3-solidity-lib/contracts/ProtobufLib.sol\";")
+	b.P()
+
 	for i := 0; i < len(protoFile.GetMessageType()); i++ {
 		err := generateMessage(protoFile.GetMessageType()[i], b)
 		if err != nil {
@@ -58,6 +66,7 @@ func generateFile(protoFile *descriptorpb.FileDescriptorProto) (*pluginpb.CodeGe
 	}
 
 	// TODO add b to response
+	println(b.String())
 
 	return responseFile, nil
 }
@@ -65,6 +74,20 @@ func generateFile(protoFile *descriptorpb.FileDescriptorProto) (*pluginpb.CodeGe
 func generateMessage(descriptor *descriptorpb.DescriptorProto, b *WriteableBuffer) error {
 	println(descriptor.GetName())
 	// println(descriptor.GetField()[0].String())
+
+	// TODO generate header, imports, and struct definition
+	structName := descriptor.GetName()
+	err := checkKeyword(structName)
+	if err != nil {
+		return err
+	}
+
+	b.P(fmt.Sprintf("struct %s {", structName))
+
+	// TODO loop over fields
+
+	b.P("}")
+	b.P()
 
 	return nil
 }
@@ -75,4 +98,43 @@ func checkSyntaxVersion(v string) error {
 	}
 
 	return errors.New("Must use syntax = \"proto3\";")
+}
+
+func checkKeyword(w string) error {
+	switch w {
+	case "after",
+		"alias",
+		"apply",
+		"auto",
+		"case",
+		"copyof",
+		"default",
+		"define",
+		"final",
+		"immutable",
+		"implements",
+		"in",
+		"inline",
+		"let",
+		"macro",
+		"match",
+		"mutable",
+		"null",
+		"of",
+		"partial",
+		"promise",
+		"reference",
+		"relocatable",
+		"sealed",
+		"sizeof",
+		"static",
+		"supports",
+		"switch",
+		"typedef",
+		"typeof",
+		"unchecked":
+		return errors.New("Using Solidity keyword " + w)
+	}
+
+	return nil
 }
