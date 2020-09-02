03e775a4a5f0e2c80a8ae1bfcdc03f0941a50988
John Adler <john.adler@protonmail.ch>
2020-09-02 13:04:55 -0400

    Loop over messages.

--- generator/generator.go
+++ generator/generator.go
@@ -3,6 +3,7 @@ package generator
 import (
 	"errors"
 
+	"google.golang.org/protobuf/types/descriptorpb"
 	"google.golang.org/protobuf/types/pluginpb"
 )
 
@@ -24,7 +25,7 @@ func New(request *pluginpb.CodeGeneratorRequest) *Generator {
 // Generate generates Solidity code from the requested .proto files.
 func (g *Generator) Generate() ([]*pluginpb.CodeGeneratorResponse_File, error) {
 	for i := 0; i < len(g.request.GetProtoFile()); i++ {
-		_, err := g.generateFile(i)
+		_, err := generateFile(g.request.GetProtoFile()[i])
 		if err != nil {
 			return nil, err
 		}
@@ -34,17 +35,34 @@ func (g *Generator) Generate() ([]*pluginpb.CodeGeneratorResponse_File, error) {
 }
 
 // generateFile generates Solidity code from a single .proto file.
-func (g *Generator) generateFile(fileIndex int) (*pluginpb.CodeGeneratorResponse_File, error) {
-	err := checkSyntaxVersion(g.request.GetProtoFile()[fileIndex].GetSyntax())
+func generateFile(protoFile *descriptorpb.FileDescriptorProto) (*pluginpb.CodeGeneratorResponse_File, error) {
+	err := checkSyntaxVersion(protoFile.GetSyntax())
 	if err != nil {
 		return nil, err
 	}
+
+	responseFile := &pluginpb.CodeGeneratorResponse_File{}
+
+	for i := 0; i < len(protoFile.GetMessageType()); i++ {
+		err := generateMessage(protoFile.GetMessageType()[i], responseFile)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return nil, nil
+}
+
+func generateMessage(descriptor *descriptorpb.DescriptorProto, responseFile *pluginpb.CodeGeneratorResponse_File) error {
+	println(descriptor.GetName())
+
+	return nil
 }
 
 func checkSyntaxVersion(v string) error {
 	if v == "proto3" {
 		return nil
 	}
+
 	return errors.New("Must use syntax = \"proto3\";")
 }
