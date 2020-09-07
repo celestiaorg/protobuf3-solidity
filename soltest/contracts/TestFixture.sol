47b74aa9bf00ffd0bbb3669a2f59dc7320d0d45a
John Adler <john.adler@protonmail.ch>
2020-09-07 00:31:48 -0400

    Import all features generated .sol.

--- soltest/contracts/TestFixture.sol
+++ soltest/contracts/TestFixture.sol
@@ -2,5 +2,6 @@
 pragma solidity >=0.6.0 <8.0.0;
 
 import "@lazyledger/protobuf3-solidity-lib/contracts/ProtobufLib.sol";
+import "../../test/pass/all_features/all_features.proto.sol";
 
 contract TestFixture {}
